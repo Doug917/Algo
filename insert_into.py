2bd53990935933a4d00ad1280c665c10b607b339
Doug <123341895+Doug917@users.noreply.github.com>
2023-12-09 19:39:50 -0700

    Create insert_into.py

---
+++ insert_into.py
@@ -0,0 +1,21 @@
+# Insert element at position i<=len(arr) of array arr.
+arr = [1,3,7,9,1]
+i = 3
+el = 0
+def insert_arr(arr, i, element):
+    
+    j = 0
+    while j < i:
+        j += 1
+        
+    arr.append(0)
+    last = len(arr) - 1
+    while last > j:
+        arr[last] = arr[last-1]
+        last -= 1
+        
+    arr[j] = element
+    print(arr)
+
+#test
+insert_arr(arr, i, el)
